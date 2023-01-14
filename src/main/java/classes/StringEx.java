28b0ce773c266ada882d2c7a598b074c1780598e
halaalazzawi <halaalazzawi@halas-air-2>
2023-01-13 19:34:59 -0500

    Reverse method

--- src/main/java/classes/StringEx.java
+++ src/main/java/classes/StringEx.java
@@ -21,6 +21,12 @@ public class StringEx {
 	}
 	
 	public static void reverse(String str) {
+		  String reverse = "";
+		    for(int i = str.length() - 1; i >= 0; i--) {
+		        reverse = reverse + str.charAt(i);
+		    }
+		    
+		
 		
 	}
 	
